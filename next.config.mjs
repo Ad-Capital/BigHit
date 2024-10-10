024693489b2aa42f99533ce51465bde1764d7d4c
Trader_AD <adxkap@gmail.com>
2024-10-10 08:38:53 +0100

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
-export default {
-  output: 'export', // Enable static export
-  images: {
-    unoptimized: true, // Disable image optimization (GitHub Pages doesn't support it)
-  },
-  basePath: '/Codeland', // Set the repository name as the base path
-  assetPrefix: '/Codeland/', // Set asset prefix for correct asset loading
+/** @type {import('next').NextConfig} */
+const nextConfig = {
 };
+
+export default nextConfig;
