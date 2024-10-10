738a894dae5647ab57c9dbe2de78e4331fbe7122
Trader_AD <adxkap@gmail.com>
2024-10-10 08:04:30 +0100

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  };
-  
-  export default nextConfig;
+export default {
+  output: 'export', // Enable static export
+  images: {
+    unoptimized: true, // Disable image optimization (GitHub Pages doesn't support it)
+  },
+  basePath: '/repository-name', // Set the repository name as the base path
+  assetPrefix: '/repository-name/', // Set asset prefix for correct asset loading
+};
